79638b79200a08656558557c9c73dbdf3d0890f2
kavhnr <kavhnr@gmail.com>
2026-02-18 19:54:02 -0700

    update homebrew formula for v0.1.1

--- packaging/homebrew/oclean.rb
+++ packaging/homebrew/oclean.rb
@@ -1,20 +1,20 @@
 class Oclean < Formula
   desc "Process-cleanup wrapper for opencode sessions"
   homepage "https://github.com/kavhnr/oclean"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kavhnr/oclean/releases/download/v0.1.0/oclean-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "083c6c8bd086a3e474235e2ad1a5a0204a73251df5dfa8fb8009c23ca9b66a00"
+      url "https://github.com/kavhnr/oclean/releases/download/v0.1.1/oclean-v0.1.1-aarch64-apple-darwin.tar.gz"
+      sha256 "f91bd71d6e8c0dc0d3228c03fac0873a3db74fbc16bf81d29c8fc8d006db7521"
     else
       odie "x86_64 macOS package is not published yet"
     end
   end
 
   on_linux do
-    url "https://github.com/kavhnr/oclean/releases/download/v0.1.0/oclean-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "495f121f96ec93a9d9de661c701ed6807da36d575dc135b9929ffcb59fabe456"
+    url "https://github.com/kavhnr/oclean/releases/download/v0.1.1/oclean-v0.1.1-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "65388338d840483ee6502d89cae6f8b082a36440574630d5c0a0abcf736d00b9"
   end
 
   def install
